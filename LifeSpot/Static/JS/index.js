bcd2a5c9bdfbbbf0b03d0344b8495069becd459f
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-11-30 02:36:17 +0500

    Задание 29.1.2

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,14 +1,4 @@
-﻿let age = prompt("Пожалуйста, введите ваш возраст");
-
-if (age >= 18) {
-    alert("Приветствуем на LifeSpot! " + new Date().toLocaleString());
-}
-else {
-    alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-    window.location.href = "http://www.google.com"
-}
-
-function filterContent() {
+﻿function filterContent() {
     let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
     let elements = document.getElementsByClassName('video-container');
     for (let i = 0; i <= elements.length; i++) {
@@ -18,5 +8,24 @@ function filterContent() {
         } else {
             elements[i].style.display = 'inline-block';
         }
+    }
+}
+
+function handleSession() {
+    let session = new Map();
+    session.set("userAgent", window.navigator.userAgent)
+    session.set("age", prompt("Пожалуйста, введите ваш возраст"))
+    if (session.get("age") >= 18) {
+        let startDate = new Date().toLocaleString();
+
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
+        session.set("startDate", startDate)
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+    for (let result of session) {
+        console.log(result)
     }
 }
