9da9112fd730476d7eb294ca26d513dfd378ea8e
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-11-30 02:48:19 +0500

    Задание 29.3.8

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -10,11 +10,25 @@
     }
 }
 
+// Логирование сессии (объявлено через expression)
+let sessionLog = function logSession(session) {
+    // Вывод в консоль
+    for (let result of session) {
+        console.log(result)
+    }
+}
 
-let sessionHandler = function handleSession() {
+// Обработка сессии (объявлено через declaration)
+function handleSession() {
+    // создадим объект Map для хранения сессии
     let session = new Map();
+    // Сохраним UserAgent
     session.set("userAgent", window.navigator.userAgent)
-    session.set("age", prompt("Пожалуйста, введите ваш возраст"))
+
+    // Запросим возраст пользователя и тоже сохраним
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+
+    // Проверка на возраст и сохранение сессии
     if (session.get("age") >= 18) {
         let startDate = new Date().toLocaleString();
 
@@ -25,7 +39,7 @@ let sessionHandler = function handleSession() {
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
         window.location.href = "http://www.google.com"
     }
-    for (let result of session) {
-        console.log(result)
-    }
+
+    // Теперь мы возвращаем объект сессии
+    return session;
 }
