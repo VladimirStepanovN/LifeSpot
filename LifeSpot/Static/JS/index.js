c1c8bd7ee9cc17a722b14716b8d919a03000543b
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-11-30 02:32:35 +0500

    Задание 29.1.1

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -7,3 +7,16 @@ else {
     alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
     window.location.href = "http://www.google.com"
 }
+
+function filterContent() {
+    let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
+    let elements = document.getElementsByClassName('video-container');
+    for (let i = 0; i <= elements.length; i++) {
+        let videoText = elements[i].querySelector('.video-title').innerText;
+        if (!videoText.toLowerCase().includes(inputString.toLowerCase())) {
+            elements[i].style.display = 'none';
+        } else {
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
