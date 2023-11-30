41d59f59b59e793fd10a7bb8264f5164141a099a
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-11-30 23:54:59 +0500

    Задание 29.5.5

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,4 +1,36 @@
-﻿function filterContent() {
+﻿/* Сессия теперь создается в общей области видимости.
+* Будет "захватываться" тремя функциями*/
+let session = new Map();
+
+//Сохранение данных сессии сразу при заходе пользователя на страницу
+function handleSession() {
+    // Сохраним время начала сессии
+    session.set("startDate", new Date().toLocaleString())
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent)
+}
+
+//Проверка возраста пользователя
+function checkAge() {
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+
+    if (session.get("age") >= 18) {
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+
+//Вывод данных сессии в консоль
+let sessionLog = function logSession() {
+    for (let result of session) {
+        console.log(result)
+    }
+}
+
+function filterContent() {
     let elements = document.getElementsByClassName('video-container');
     for (let i = 0; i <= elements.length; i++) {
         let videoText = elements[i].querySelector('.video-title').innerText;
@@ -8,38 +40,4 @@
             elements[i].style.display = 'inline-block';
         }
     }
-}
-
-// Логирование сессии (объявлено через expression)
-let sessionLog = function logSession(session) {
-    // Вывод в консоль
-    for (let result of session) {
-        console.log(result)
-    }
-}
-
-// Обработка сессии (объявлено через declaration)
-function handleSession() {
-    // создадим объект Map для хранения сессии
-    let session = new Map();
-    // Сохраним UserAgent
-    session.set("userAgent", window.navigator.userAgent)
-
-    // Запросим возраст пользователя и тоже сохраним
-    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
-
-    // Проверка на возраст и сохранение сессии
-    if (session.get("age") >= 18) {
-        let startDate = new Date().toLocaleString();
-
-        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
-        session.set("startDate", startDate)
-    }
-    else {
-        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-        window.location.href = "http://www.google.com"
-    }
-
-    // Теперь мы возвращаем объект сессии
-    return session;
 }
